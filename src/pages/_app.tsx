28ce12ad9b6ff2d66b0875c900ca61c09c038409
Billy Huang <10646003@ntub.edu.tw>
2023-06-17 13:12:00 +0800

    feat(app): setup chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import Layout from '@/components/Layout';
+import { theme } from '@/fixtures/theme';
 import { queryClient } from '@/helpers/query-client';
 import wrapper from '@/redux/store';
 
@@ -20,9 +22,11 @@ const App = ({ Component, ...pageProps }: AppProps) => {
         <Head>
           <title>Next.js Template</title>
         </Head>
-        <Layout>
-          <Component {...props.pageProps} />
-        </Layout>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...props.pageProps} />
+          </Layout>
+        </ChakraProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ReduxProvider>
