d84f32a54648213067bcc272b13b414d57fd60c6
Billy Huang <10646003@ntub.edu.tw>
2024-02-18 13:25:33 +0800

    feat(app): setup react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,31 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { Provider } from 'react-redux';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import Layout from '@/components/Layout';
+import { queryClient } from '@/helpers/query-client';
 import wrapper from '@/redux/store';
+
 import '@/styles/globals.scss';
 
 const App = ({ Component, ...pageProps }: AppProps) => {
   const { store, props } = wrapper.useWrappedStore(pageProps);
 
   return (
-    <Provider store={store}>
-      <Head>
-        <title>Next.js Template</title>
-      </Head>
-      <Layout>
-        <Component {...props.pageProps} />
-      </Layout>
-    </Provider>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>Next.js Template</title>
+        </Head>
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ReduxProvider>
   );
 };
 
