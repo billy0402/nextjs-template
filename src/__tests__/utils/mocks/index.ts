1f29f9f9da617df7cae9efb7eb206b8d03d28cc5
Billy Huang <10646003@ntub.edu.tw>
2024-02-18 13:27:19 +0800

    test(msw): setup

---
+++ src/__tests__/utils/mocks/index.ts
@@ -0,0 +1,13 @@
+export async function initMocks() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server');
+    server.listen();
+  } else {
+    const { worker } = await import('./browser');
+    worker.start();
+  }
+}
